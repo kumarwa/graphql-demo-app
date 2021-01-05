5788a36ed71457789ff4f52df95f5a15d08fa49f
Kumar Rajasekaran <krajasek@starbucks.com>
2021-01-05 11:07:55 -0800

    fix-graphqlapi-folder-issue

---
+++ GraphQLAPI/resolvers.js
@@ -0,0 +1,16 @@
+const Query = require('./resolvers/query');
+const Session = require('./resolvers/session');
+const Mutation = require('./resolvers/mutation');
+module.exports = {
+  Query,
+  Session,
+  Mutation,
+  SessionOrError: {
+    __resolveType(obj) {
+      if (obj.code) {
+        return 'Error';
+      }
+      return 'Session';
+    },
+  },
+};
